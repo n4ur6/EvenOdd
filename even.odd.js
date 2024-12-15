72bdb8a10cd0d7fd2d0f33754ceb4a49349816f1
n4ur6 <naqeeburrehman783@gmail.com>
2024-12-16 01:07:53 +0500

    Add files via upload

---
+++ even.odd.js
@@ -0,0 +1,28 @@
+var dictionary = {
+    starvation : "Macabre" ,
+    starvation : "Shockingly repellent or inspiring horror" ,
+    }
+ 
+    console.log(dictionary.starvation);
+ 
+   
+ var dictionary = {
+         starvation : "Machination" ,
+         starvation : "A crafty and involved plot to achieve your ends" ,
+ } 
+    console.log(dictionary.starvation);
+ 
+ 
+   
+    var dictionary = {
+       starvation : "Malignant" ,
+       starvation : "Evil or harmful" ,
+ } 
+  console.log(dictionary.starvation);
+ 
+ 
+  var dictionary = {
+    starvation : "Arrant" ,
+    starvation : "Complete and wholly" ,
+ } 
+ console.log(dictionary.starvation);
